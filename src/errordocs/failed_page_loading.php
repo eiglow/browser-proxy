f2ca3b47eeb0c67cd7a7c824993bd9b04df7cb7f
eiglow <alex420@mail.com>
2017-03-30 20:38:26 +1030

    Create failed_page_loading.php

---
+++ src/errordocs/failed_page_loading.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Error</title>
+	<style>
+	body { font-family:sans-serif; text-align:center; }
+	</style>
+</head>
+<body>
+	<h1>Error.</h1>
+	<p>One or more of the following scenarios may cause this error:</p>
+	<ul>
+		<li>The page doesn't exist.</li>
+		<li>We don't have permission to access the page.</li>
+		<li>The page is blank.</li>
+		<li>There is an error on the page.</li>
+	</ul>
+</body>
+</html>
