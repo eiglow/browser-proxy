d69e0d04020fb75d18abebf3a9b1a1227c4e1165
eiglow <alex420@mail.com>
2017-07-06 19:07:00 +0930

    Fixed some pretty big bugs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const HOMEPAGE = "http://www.google.com"; // TODO: maybe use cookies to set homepage?
+const LOGGING = true;
 var HISTORY = [];
 
 // ================================================================
@@ -28,82 +29,93 @@ var UIElems = {
 // Regex from http://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript#answer-21553982
 function prettifyURL(toPretty) {
 	
-	console.log("Prettifying " + toPretty);
+	if (LOGGING) console.log("Prettifying " + toPretty);
 	
 	toPretty = toPretty.replace(/\r?\n|\r/g, "");
 	var match = toPretty.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
     
 	return "<span class='url-faded'>" + match[1] + "//</span>" + match[3] +
-	(match[4] ? "<span class='url-faded'>:" + match[4] + "</span>" : "")
-	+ match[5] + "<span class='url-faded'>" + match[6] + match[7];
+	(match[4] ? "<span class='url-faded'>:" + match[4] + "</span>" : "") +
+	match[5] + "<span class='url-faded'>" + match[6] + match[7];
 	
 }
 
 // Interface for the URL bar. Reasoning for this being a class is to make it into tabs later
-// Get the current URL with curBrowserState.curPage()
-// Set it with curBrowserState.curPage("http://example.com")
-function browserState() {
-	this.privateCurPage = "";
-	this.curPage = function(newPage) {
+// Get the current URL with curTab.curPage
+// Set it with curTab.setPage("http://example.com")
+function Tab() {
+	this.curPage = "";
+	this.setPage = function(newPage) {
+		if (LOGGING) console.log("browserState curPage to '" + newPage + "'");
 		if (newPage) {
-			this.privateCurPage = newPage;
+			this.curPage = newPage;
 			UIElems.url.innerHTML = prettifyURL(newPage);
 		}
-		return this.privateCurPage;
 	};
-};
-var curBrowserState = new browserState();
+}
+var curTab = new Tab();
 
 // Processes the links in the page within the frame.
 // TODO: review and test
-function internalNavigate(url) {
-	curBrowserState.curPage(url);
-	navigateTo();
+function processIndividualLink(linkk) {
+	var tempurl = linkk.href;
+	if (tempurl) {
+		linkk.href = ""; // don't remove the href tag because it won't look like a link anymore
+		linkk.addEventListener("click", function() {
+			if (LOGGING) console.log("internal navigate to '" + tempurl + "'");
+			curTab.setPage(tempurl);
+			navigateTo();
+		});
+	}
 }
 function processLinks() {
-	console.log("Processing links...");
+	if (LOGGING) console.log("Processing links...");
 	var innerDoc = UIElems.frame.contentDocument || UIElems.frame.contentWindow.document;
 	console.log(innerDoc);
 	var links = innerDoc.getElementsByTagName("a");
 	console.log(links);
 	for (var i = 0; i < links.length; i++) {
-		let tempurl = links[i].href;
-		if (tempurl) {
-			links[i].href = ""; // don't remove the href tag because it won't look like a link anymore
-			links[i].addEventListener("click", internalNavigate.call(tempurl));
-		}
+		processIndividualLink(links[i]);
 	}
 }
 
 // Navigation, differs from refresh, back, and forward functions.
-// Set the page with curBrowserState.curPage("http://example.com") before calling this function
+// Set the page with curTab.setPage("http://example.com") before calling this function
 function navigateTo() {
-	if (url != curBrowserState.curPage()) HISTORY.push(curBrowserState.curPage());
-	page(curBrowserState.curPage());
+	if (LOGGING) console.log("navigateTo");
+	HISTORY.push(curTab.curPage);
+	page(curTab.curPage);
 }
 
+// Reloads the page
 function refresh() {
+	if (LOGGING) console.log("refreshing");
 	page(
-		curBrowserState.curPage()
+		curTab.curPage
 	);
 }
 function back() {
+	if (LOGGING) console.log("going back");
 	// checks if the previous item of history exists
-	if (HISTORY[HISTORY.indexOf(curBrowserState.curPage()) - 1])
+	if (HISTORY[HISTORY.indexOf(curTab.curPage) - 1])
 		page(
-			HISTORY[HISTORY.indexOf(curBrowserState.curPage()) - 1]
+			HISTORY[HISTORY.indexOf(curTab.curPage) - 1]
 		);
 }
 function forward() {
+	if (LOGGING) console.log("going forward");
 	// checks if the next item of history exists
-	if (HISTORY[HISTORY.indexOf(curBrowserState.curPage()) + 1])
+	if (HISTORY[HISTORY.indexOf(curTab.curPage) + 1])
 		page(
-			HISTORY[HISTORY.indexOf(curBrowserState.curPage()) + 1]
+			HISTORY[HISTORY.indexOf(curTab.curPage) + 1]
 		);
 }
 
 var GETData = [];
+// Loads a page inside the iframe
 function page(url, scriptsState, cookiesState, objectsState) {
+	
+	if (LOGGING) console.log("page '" + url + "'");
 	
 	scriptsState = scriptsState || pageSettings[0].className;
 	cookiesState = cookiesState || pageSettings[1].className;
@@ -113,8 +125,8 @@ function page(url, scriptsState, cookiesState, objectsState) {
 	GETData.scripts = pageSettings[0].classList.contains("enabled");
 	GETData.cookies = pageSettings[1].classList.contains("enabled");
 	GETData.objects = pageSettings[2].classList.contains("enabled");
-	GETData.base    = encodeURIComponent(window.btoa(curBrowserState.curPage()));
-	curBrowserState.curPage(url);
+	GETData.base    = encodeURIComponent(window.btoa(curTab.curPage));
+	//curTab.setPage(url);
 	UIElems.frame.src = "page.php?url=" + GETData.url +
 	"&scripts=" + GETData.scripts +
 	"&cookies=" + GETData.cookies +
@@ -134,7 +146,7 @@ function toggleButton() {
 
 for (let i = 0; i < 3; i++) {
 	pageSettings[i].addEventListener("click", toggleButton);
-};
+}
 UIElems.refresh.addEventListener("click", refresh);
 UIElems.back.addEventListener("click", back);
 UIElems.forward.addEventListener("click", forward);
@@ -142,17 +154,18 @@ UIElems.forward.addEventListener("click", forward);
 UIElems.url.addEventListener("keypress", function(e) {
 	if (e.keyCode == 13) { // if enter pressed
 		e.preventDefault();
-		curBrowserState.curPage(UIElems.url.innerText);
+		curTab.setPage(UIElems.url.innerText.trim());
 		navigateTo();
 	}
 });
 
 UIElems.frame.addEventListener("load", function() {
-	console.log("Frame page loaded");
+	if (LOGGING) console.log("Frame page loaded");
 	processLinks();
 });
 function init() {
-	curBrowserState.curPage(HOMEPAGE);
+	if (LOGGING) console.log("initialising");
+	curTab.setPage(HOMEPAGE);
 	navigateTo();
 }
 top.addEventListener("load", init);
